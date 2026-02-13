fbcc5e5b09a2b29647a76e75a0e95344f11a9b04
edificiomateusadm-jpg <edificiomateus.adm@gmail.com>
2026-02-12 19:40:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from supabase import create_client, Client
 
 # --- CONFIGURACIÓN ---
 # Reemplaza con tus datos de Supabase
-SUPABASE_URL = "https://supabase.com/dashboard/project/lcktyhjugquwbtwlhhav"
-SUPABASE_KEY = "sb_publishable_w3ICwBwmfgWNrkH0dJo2-w_arDBgA20"
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 IMG_BB_API_KEY = "4d082bcad64d3390228ec3d92cdc15c3"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
@@ -134,4 +134,5 @@ else:
                 registrar_db({"dpto": st.session_state.user, "monto": mon, "tipo": "PAGO_VECINO", "mes_anio": mes_anio_v, "link_comprobante": res_img['data']['url']})
                 st.rerun()
         else: st.warning("No hay deuda cargada para este mes.")
+
 
